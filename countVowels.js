fd64beebaba7f7f6d28abecf248ba78b6ce52a11
kalaumapathy <50033847+kalaumapathy@users.noreply.github.com>
2026-07-01 15:33:30 -0500

    refactor the code

--- countVowels.js
+++ countVowels.js
@@ -1,5 +1,19 @@
 function countVowels(str) {
-    return 2;
+
+    if (typeof str !== "string") {
+        return 0;
+    }
+
+    const vowels = "aeiou";
+    let count = 0;
+
+    for (const letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 module.exports = countVowels;
